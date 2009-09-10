4313c9e0e2341ec85d673990d4b8304b88a54349
robot.johnston <robot.johnston@4bd619cc-97e9-11de-b255-e1213ca11573>
2009-09-10 16:00:34 +0000

    Committed full src dir, build 1.0.2.3

--- trunk/chrome/content/Bxt.js
+++ trunk/chrome/content/Bxt.js
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License along with Bxt
 
 Bxt = {
 	
-	version: "1.0.2.2",
+	version: "1.0.2.3",
 	
 	boot: function() {
 		window.addEventListener("DOMContentLoaded",function(e) {
