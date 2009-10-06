85886c84f831db596a4120863b5218c9ab9f7654
robot.johnston <robot.johnston@4bd619cc-97e9-11de-b255-e1213ca11573>
2009-10-06 13:10:47 +0000

    Committed full src dir, build 1.0.3.6

--- trunk/chrome/content/Bxt.Services.js
+++ trunk/chrome/content/Bxt.Services.js
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License along with Bxt
 Bxt.Services = {
 
 	handleRequest: function(e) {
+		Bxt.console.logStringMessage("serviceRequest: "+e.target.wrappedJSObject.serviceRequest.toSource());
 		Bxt.Services.public[e.target.wrappedJSObject.serviceRequest.service](e.target.wrappedJSObject);
 	},
 
--- trunk/chrome/content/Bxt.js
+++ trunk/chrome/content/Bxt.js
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License along with Bxt
 
 Bxt = {
 	
-	version: "1.0.3.5",
+	version: "1.0.3.6",
 	
 	boot: function() {
 		window.addEventListener("DOMContentLoaded",function(e) {
