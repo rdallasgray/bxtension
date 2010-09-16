2ffd6c915733a112fc0ad7459e2e4da8b40fcd0e
robot.johnston <robot.johnston@4bd619cc-97e9-11de-b255-e1213ca11573>
2010-09-16 14:20:33 +0000

    --username

--- trunk/chrome/content/Bxt.js
+++ trunk/chrome/content/Bxt.js
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License along with Bxt
 
 Bxt = {
 	
-	version: "1.0.6.3",
+	version: "1.0.6.4",
 	
 	boot: function() {
 		window.addEventListener("DOMContentLoaded",function(e) {
