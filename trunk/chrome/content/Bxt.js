5d10c62a791a3bb6d4633c0b80fb5720d7da15d5
robot.johnston <robot.johnston@4bd619cc-97e9-11de-b255-e1213ca11573>
2012-01-16 09:41:20 +0000

    New xpi version

--- trunk/chrome/content/Bxt.js
+++ trunk/chrome/content/Bxt.js
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License along with Bxt
 
 Bxt = {
 	
-	version: "1.0.7.2",
+	version: "1.0.7.3",
 	
 	boot: function() {
 		window.addEventListener("DOMContentLoaded",function(e) {
